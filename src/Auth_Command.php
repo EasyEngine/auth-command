fc62c8feeb707f8ee5b5c5ada1f04e2bdbc00506
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-16 20:30:25 +0530

    Add whitelist create,append,list and remove functions
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/Auth_Command.php
+++ src/Auth_Command.php
@@ -153,11 +153,161 @@ class Auth_Command extends EE_Command {
 	}
 
 	/**
+	 * create, append, remove, list ip whitelisting for a site or globally.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [<create>]
+	 * : Create ip whitelisting for a site or globally.
+	 *
+	 * [<append>]
+	 * : Append ips in whitelisting of a site or globally.
+	 *
+	 * [<list>]
+	 * : List whitelisted ip's of a site or of global scope.
+	 *
+	 * [<remove>]
+	 * : Remove whitelisted ip's of a site or of global scope.
+	 *
+	 * [<site-name>]
+	 * : Name of website to be secured / `global` for global scope.
+	 *
+	 * [--ip=<ip>]
+	 * : Comma seperated ips.
+	 */
+	public function whitelist( $args, $assoc_args ) {
+
+		$commands = [ 'create', 'append', 'list', 'remove' ];
+		if ( ! ( isset( $args[0] ) && in_array( $args[0], $commands ) ) ) {
+			$help = PHP_EOL;
+			foreach ( $commands as $command ) {
+				$help .= "ee auth whitelist $command [<site-name>/global] [--ip=<ip>]" . PHP_EOL;
+			}
+			EE::error( 'Please use valid command syntax. You can use:' . $help );
+
+		}
+
+		$command = array_shift( $args );
+		$global  = false;
+		if ( isset( $args[0] ) && 'global' === $args[0] ) {
+			$this->site['name'] = $args[0];
+			$global             = true;
+		} else {
+			$args = EE\SiteUtils\auto_site_name( $args, 'auth', __FUNCTION__ . ' ' . $command );
+			$this->populate_site_info( $args );
+		}
+
+		$ip = EE\Utils\get_flag_value( $assoc_args, 'ip' );
+
+		$file         = EE_CONF_ROOT . '/nginx/vhost.d/';
+		$file         .= $global ? 'default_acl' : $this->site['name'] . '_acl';
+		$user_ips     = array_filter( explode( ',', $ip ), 'strlen' );
+		$existing_ips = $this->get_ips_from_file( $global );
+
+		call_user_func_array( [ $this, "whitelist_$command" ], [ $file, $user_ips, $existing_ips ] );
+
+		$this->reload();
+	}
+
+	/**
+	 * Function to create whitelist file.
+	 *
+	 * @param string $file        The whitelisting file.
+	 * @param array $user_ips     ip's provided by the user.
+	 * @param array $existing_ips Existing ip's in the given file.
+	 */
+	private function whitelist_create( $file, $user_ips, $existing_ips ) {
+
+		foreach ( $user_ips as $ip ) {
+			$file_content = "allow $ip;" . PHP_EOL;
+		}
+		$this->fs->dumpFile( $file, $file_content );
+	}
+
+	/**
+	 * Function to append to whitelist file.
+	 *
+	 * @param string $file        The whitelisting file.
+	 * @param array $user_ips     ip's provided by the user.
+	 * @param array $existing_ips Existing ip's in the given file.
+	 */
+	private function whitelist_append( $file, $user_ips, $existing_ips ) {
+
+		$all_ips      = array_unique( array_merge( $user_ips, $existing_ips ) );
+		$file_content = '';
+		foreach ( $all_ips as $individual_ip ) {
+			$file_content .= "allow $individual_ip;" . PHP_EOL;
+		}
+		$this->fs->dumpFile( $file, $file_content );
+	}
+
+	/**
+	 * Function to list whitelisted ips.
+	 *
+	 * @param string $file        The whitelisting file.
+	 * @param array $user_ips     ip's provided by the user.
+	 * @param array $existing_ips Existing ip's in the given file.
+	 */
+	private function whitelist_list( $file, $user_ips, $existing_ips ) {
+
+		if ( ! empty( $existing_ips ) ) {
+			EE::log( 'Whitelisted IPs for %s scope', $this->site['name'] );
+			foreach ( $existing_ips as $ips ) {
+				EE::log( $ips );
+			}
+		}
+	}
+
+	/**
+	 * Function to remove whitelisted ips.
+	 *
+	 * @param string $file        The whitelisting file.
+	 * @param array $user_ips     ip's provided by the user.
+	 * @param array $existing_ips Existing ip's in the given file.
+	 */
+	private function whitelist_remove( $file, $user_ips, $existing_ips ) {
+
+		if ( empty( $user_ips ) || 'all' === $user_ips[0] ) {
+			$this->fs->remove( $file );
+		} else {
+			$updated_ips  = array_diff( $existing_ips, $user_ips );
+			$file_content = '';
+			foreach ( $updated_ips as $individual_ip ) {
+				$file_content .= "allow $individual_ip;" . PHP_EOL;
+			}
+			$this->fs->dumpFile( $file, $file_content );
+		}
+	}
+
+
+	/**
 	 * Function to reload the global reverse proxy to update the effect of changes done.
 	 */
 	private function reload() {
 
 		EE::exec( sprintf( 'docker exec %s sh -c "/app/docker-entrypoint.sh /usr/local/bin/docker-gen /app/nginx.tmpl /etc/nginx/conf.d/default.conf; /usr/sbin/nginx -s reload"', EE_PROXY_TYPE ) );
+	}
+
+	/**
+	 * Function to get the list of ip's from given file.
+	 *
+	 * @param boolean $global Is the scope global or site specific.
+	 *
+	 * @return array of existing ips.
+	 */
+	private function get_ips_from_file( $global ) {
+
+		$file         = EE_CONF_ROOT . '/nginx/vhost.d/';
+		$file         .= $global ? 'default_acl' : $this->site['name'] . '_acl';
+		$existing_ips = [];
+		if ( $this->fs->exists( $file ) ) {
+			$existing_ips_in_file = array_filter( explode( PHP_EOL, trim( file_get_contents( $file ) ) ), 'strlen' );
+			foreach ( $existing_ips_in_file as $ip_in_file ) {
+				$existing_ips[] = str_replace( [ 'allow ', ';' ], '', trim( $ip_in_file ) );
+			}
+		}
+
+		return $existing_ips;
 	}
 
 	/**
