dc8dd448156fa806a52bcc04b80aaa88b4713ebd
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-16 20:26:17 +0530

    Add create function Add variables Add method to poupulate basic site info

--- src/Auth_Command.php
+++ src/Auth_Command.php
@@ -14,76 +14,95 @@
  * @package ee-cli
  */
 
-use EE\Utils;
+use \Symfony\Component\Filesystem\Filesystem;
 
 class Auth_Command extends EE_Command {
-	private $user;
-	private $pass;
-	private $site_name;
-	private $db;
 
 	/**
-	 * Executes wp-cli command on a site.
+	 * @var Filesystem $fs Symfony Filesystem object.
+	 */
+	private $fs;
+
+	/**
+	 * @var array $site Associative array containing essential site related information.
+	 */
+	private $site;
+
+	private $db;
+
+	public function __construct() {
+
+		$this->fs = new Filesystem();
+		$this->db = EE::db();
+	}
+
+
+	/**
+	 * Creates/Updates http auth for a site.
 	 *
 	 * ## OPTIONS
 	 *
-	 * <site-name>
+	 * [<site-name>]
 	 * : Name of website to be secured.
 	 *
-	 * [--auth]
-	 * : Add http auth to entire site + admin tools.
-	 *
-	 * [<user>]
+	 * [--user=<user>]
 	 * : Username for http auth.
 	 *
-	 * [<pass>]
+	 * [--pass=<pass>]
 	 * : Password for http auth
+	 *
+	 * @alias update
 	 */
-	public function __invoke( $args, $assoc_args ) {
-		$this->db        = EE::db();
-		$this->site_name = $args[0];
-		EE::debug( "ee secure start, Site: $this->site_name" );
-		if ( ! $this->db::site_in_db( $this->site_name ) ) {
-			EE::error( "No site with name `$this->site_name` found." );
+	public function create( $args, $assoc_args ) {
+
+		$args = EE\SiteUtils\auto_site_name( $args, 'auth', __FUNCTION__ );
+		$this->populate_site_info( $args );
+
+		EE::debug( sprintf( 'ee auth start, Site: %s', $this->site['name'] ) );
+
+		$user = EE\Utils\get_flag_value( $assoc_args, 'user', 'easyengine' );
+		$pass = EE\Utils\get_flag_value( $assoc_args, 'pass', EE\Utils\random_password() );
+
+		EE::debug( 'Verifying htpasswd is present.' );
+		$check_htpasswd_present = EE::exec( sprintf( 'docker exec %s sh -c \'command -v htpasswd\'', EE_PROXY_TYPE ) );
+		if ( ! $check_htpasswd_present ) {
+			EE::error( sprintf( 'Could not find apache2-utils installed in %s.', EE_PROXY_TYPE ) );
 		}
-		$global_reverse_proxy = 'ee-nginx-proxy';
+		$params = $this->fs->exists( EE_CONF_ROOT . '/nginx/htpasswd/' . $this->site['name'] ) ? 'b' : 'bc';
+		EE::exec( sprintf( 'docker exec %s htpasswd -%s /etc/nginx/htpasswd/%s %s %s', EE_PROXY_TYPE, $params, $this->site['name'], $user, $pass ) );
 
-		$auth = EE\Utils\get_flag_value( $assoc_args, 'auth' );
+		EE::log( 'Reloading global reverse proxy.' );
+		$this->reload();
 
-		if ( $auth ) {
-			$this->user = $this->get_val( $args, 1, 'user name', 'easyengine' );
-			$this->pass = $this->get_val( $args, 2, 'password', EE\Utils\random_password() );
-
-			EE::debug( 'Verifying htpasswd is present.' );
-			$check_htpasswd_present = EE\Utils\default_launch( "docker exec $global_reverse_proxy sh -c 'command -v htpasswd'" );
-			if ( ! $check_htpasswd_present ) {
-				EE::error( "Could not find apache2-utils installed in $global_reverse_proxy." );
-			}
-
-			EE::debug( 'Creating auth file.' );
-			EE\Utils\default_launch( "docker exec $global_reverse_proxy htpasswd -bc /etc/nginx/htpasswd/$this->site_name $this->user $this->pass" );
-
-			EE::log( 'Reloading global reverse proxy.' );
-			$this->reload();
-
-			EE::log( "Auth successfully added to $this->site_name" );
-		} else {
-			EE::error( 'Only --auth is supported so far.' );
-		}
-
+		EE::log( sprintf( 'Auth successfully updated for %s scope. New values added/updated:', $this->site['name'] ) );
+		EE::log( 'User:' . $user );
+		EE::log( 'Pass:' . $pass );
 	}
 
-	private function get_val( $args, $arg_pos, $prompt, $default ) {
-		if ( isset( $args[$arg_pos] ) ) {
-			return $args[$arg_pos];
-		} else {
-			$val = EE::input( "Provide HTTP authentication $prompt [$default]:" );
-
-			return empty( $val ) ? $default : $val;
-		}
-	}
-
+	/**
+	 * Function to reload the global reverse proxy to update the effect of changes done.
+	 */
 	private function reload() {
-		EE\Utils\default_launch( 'docker exec ee-nginx-proxy sh -c "/app/docker-entrypoint.sh /usr/local/bin/docker-gen /app/nginx.tmpl /etc/nginx/conf.d/default.conf; /usr/sbin/nginx -s reload"' );
+
+		EE::exec( sprintf( 'docker exec %s sh -c "/app/docker-entrypoint.sh /usr/local/bin/docker-gen /app/nginx.tmpl /etc/nginx/conf.d/default.conf; /usr/sbin/nginx -s reload"', EE_PROXY_TYPE ) );
+	}
+
+	/**
+	 * Populate basic site info from db.
+	 */
+	private function populate_site_info( $args ) {
+
+		$this->site['name'] = EE\Utils\remove_trailing_slash( $args[0] );
+
+		if ( EE::db()::site_in_db( $this->site['name'] ) ) {
+
+			$db_select = EE::db()::select( [], [ 'sitename' => $this->site['name'] ], 'sites', 1 );
+
+			$this->site['type']    = $db_select['site_type'];
+			$this->site['root']    = $db_select['site_path'];
+			$this->site['command'] = $db_select['site_command'];
+		} else {
+			EE::error( sprintf( 'Site %s does not exist.', $this->site['name'] ) );
+		}
 	}
 }
