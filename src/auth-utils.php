4622f45b50588ecf9806858427b1625359b83893
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-11-06 19:33:23 +0530

    Make logging in admin-tools auth util generation optional
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/auth-utils.php
+++ src/auth-utils.php
@@ -9,13 +9,16 @@ use EE\Model\Auth;
 /**
  * Initialize global admin tools auth if it's not present.
  *
+ * @param string $display_log Wether to display log message or not.
  * @throws \EE\ExitException
  * @throws \Exception
  */
-function init_global_admin_tools_auth() {
+function init_global_admin_tools_auth( $display_log = true ) {
 
 	if ( ! empty( Auth::get_global_admin_tools_auth() ) || ! empty( Auth::get_global_auths() ) ) {
-		EE::log( 'Global auth exists on admin-tools. Use `ee auth list global` to view credentials.' );
+		if ( $display_log ) {
+			EE::log( 'Global auth exists on admin-tools. Use `ee auth list global` to view credentials.' );
+		}
 
 		return;
 	}
@@ -32,7 +35,10 @@ function init_global_admin_tools_auth() {
 	Auth::create( $auth_data );
 
 	EE::exec( sprintf( 'docker exec %s htpasswd -bc /etc/nginx/htpasswd/default_admin_tools %s %s', EE_PROXY_TYPE, $auth_data['username'], $auth_data['password'] ) );
-	EE::success( sprintf( 'Global admin-tools auth added. Use `ee auth list global` to view credentials.' ) );
+
+	if ( $display_log ) {
+		EE::success( sprintf( 'Global admin-tools auth added. Use `ee auth list global` to view credentials.' ) );
+	}
 }
 
 /**
